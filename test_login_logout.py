f3e264deb71cbf9c53a315eef1474d8ef95ce5dc
AlexShaliapin <97082641+AlexShaliapin@users.noreply.github.com>
2024-03-27 09:53:19 +0200

    Записаний рекордером сценарій

--- test_login_logout.py
+++ test_login_logout.py
@@ -14,10 +14,10 @@ class Test_login_logout():
     self.driver.get("https://practicetestautomation.com/practice-test-login/")
     self.driver.set_window_size(1440, 821)
 
-  def login(self):
+  def login(self, username, password):
     # login
-    self.driver.find_element(By.XPATH, "//input[@id=\'username\']").send_keys("student")
-    self.driver.find_element(By.XPATH, "//input[@id=\'password\']").send_keys("Password123")
+    self.driver.find_element(By.XPATH, "//input[@id=\'username\']").send_keys(username)
+    self.driver.find_element(By.XPATH, "//input[@id=\'password\']").send_keys(password)
 
   def submit_login(self):
     # submit login
@@ -27,12 +27,11 @@ class Test_login_logout():
     # logout
     self.driver.find_element(By.XPATH, "//a[contains(text(),\'Log out\')]").click()
 
-  def teardown_method(self, method):
-    self.driver.quit()
-
-    
   def test_login_logout(self):
     self.open_home_page()
-    self.login()
+    self.login(username="student", password="Password123")
     self.submit_login()
     self.logout()
+
+  def teardown_method(self, method):
+    self.driver.quit()
