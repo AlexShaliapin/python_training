09caafa8ec6cc0a28da70839d2c6bf74e4bcfe77
AlexShaliapin <97082641+AlexShaliapin@users.noreply.github.com>
2024-03-27 09:44:46 +0200

    Записаний рекордером сценарій

--- test_login_logout.py
+++ test_login_logout.py
@@ -8,15 +8,31 @@ class Test_login_logout():
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
     self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_login_logout(self):
+
+  def open_home_page(self):
+    # open home page
     self.driver.get("https://practicetestautomation.com/practice-test-login/")
     self.driver.set_window_size(1440, 821)
+
+  def login(self):
+    # login
     self.driver.find_element(By.XPATH, "//input[@id=\'username\']").send_keys("student")
     self.driver.find_element(By.XPATH, "//input[@id=\'password\']").send_keys("Password123")
+
+  def submit_login(self):
+    # submit login
     self.driver.find_element(By.XPATH, "//button[@id=\'submit\']").click()
+
+  def logout(self):
+    # logout
     self.driver.find_element(By.XPATH, "//a[contains(text(),\'Log out\')]").click()
-  
+
+  def teardown_method(self, method):
+    self.driver.quit()
+
+    
+  def test_login_logout(self):
+    self.open_home_page()
+    self.login()
+    self.submit_login()
+    self.logout()
